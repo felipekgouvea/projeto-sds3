e596c234d68e57344d7fa791bbebb1b810e1915b
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-06 16:44:50 -0300

    Paginação

--- backend/src/main/java/com/cerg/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/cerg/dsvendas/dto/SaleDTO.java
@@ -1,5 +1,35 @@
 package com.cerg.dsvendas.dto;
 
-public class SaleDTO {
+import java.io.Serializable;
+import java.time.LocalDate;
 
+import com.cerg.dsvendas.entities.Sale;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class SaleDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	private SellerDTO seller;
+
+	public SaleDTO(Sale entity) {
+		id = entity.getId();
+		visited = entity.getVisited();
+		deals = entity.getDeals();
+		amount = entity.getAmount();
+		date = entity.getDate();
+		seller = new SellerDTO(entity.getSeller());
+	}	
 }
--- backend/src/main/java/com/cerg/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/cerg/dsvendas/services/SaleService.java
@@ -1,5 +1,31 @@
 package com.cerg.dsvendas.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cerg.dsvendas.dto.SaleDTO;
+import com.cerg.dsvendas.dto.SellerDTO;
+import com.cerg.dsvendas.entities.Sale;
+import com.cerg.dsvendas.repositorys.SaleRepository;
+import com.cerg.dsvendas.repositorys.SellerRepository;
+
+@Service
 public class SaleService {
+	
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> list = saleRepository.findAll(pageable); 
+		return list.map(x -> new SaleDTO(x));
+	}
 
 }
