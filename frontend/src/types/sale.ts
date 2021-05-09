f4e0d07fd836c695b839bb263e6eb7c365aed844
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-08 21:53:00 -0300

    Integração de Tabela

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,33 @@
-    export type SaleSum = {
-        saleName: string;
-        sum: number;
-    }
+import { Seller } from "./seller"
 
-    export type SaleSuccess = {
-        sallerName: string;
-        visited: number;
-        deals: number;
-    }
+export type Sale = {
+    id: 21,
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
+export type SaleSum = {
+    saleName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sallerName: string;
+    visited: number;
+    deals: number;
+}
